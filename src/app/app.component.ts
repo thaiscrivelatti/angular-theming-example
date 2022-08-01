a2e0cb0abd54f8ab3d71954a773e639132669656
thrivelatti <thrivelatti@gmail.com>
2022-08-01 14:06:02 -0300

    Commit inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { OverlayContainer } from '@angular/cdk/overlay';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-theming-example';
+  toggleControl = new FormControl(false);
+
+  @HostBinding('class') className = '';
+
+  constructor(private overlay: OverlayContainer) { }
+
+  ngOnInit(): void {
+      this.toggleControl.valueChanges.subscribe((darkMode) => {
+        const darkClassName = 'darkMode';
+        this.className = darkMode ? darkClassName : '';
+        if (darkMode) {
+          this.overlay.getContainerElement().classList.add(darkClassName);
+        } else {
+          this.overlay.getContainerElement().classList.remove(darkClassName);
+        }
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule} from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon'
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSlideToggleModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
